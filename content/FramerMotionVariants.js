04b24026e9f9389aa085dc59c15e77698c32d003
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-12-19 16:25:22 -0400

    Project section

--- content/FramerMotionVariants.js
+++ content/FramerMotionVariants.js
@@ -35,3 +35,21 @@ export const popUp = {
     type: "spring",
   },
 };
+
+export const opacityVariant = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { delay: 0.2 } },
+};
+
+export const fromLeftVariant = {
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      stiffness: 100,
+    },
+  },
+};
