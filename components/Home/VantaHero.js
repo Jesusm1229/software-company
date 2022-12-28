6da0dbd0045291b251b5f89fe5267ba7db06cfa5
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-12-28 17:43:55 -0400

    Adding Vanta

---
+++ components/Home/VantaHero.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from "react";
+import HALO from "vanta/dist/vanta.halo.min";
+import * as THREE from "three";
+import styles from "../../styles/Home.module.css";
+
+export default function VantaHero({ children }) {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        HALO({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+
+          maxDistance: 34.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+  return (
+    <div className={styles.container_vanta}>
+      <div className={styles.main_vanta} ref={vantaRef}>
+        {children}
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,21 @@ function MyApp({ Component, pageProps }) {
     };
   }, [router.events]);
 
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+    );
+    document.getElementsByTagName("head")[0].appendChild(threeScript);
+    return () => {
+      if (threeScript) {
+        threeScript.remove();
+      }
+    };
+  }, []);
+
   return (
     <DarkModeProvider>
       <Layout>
