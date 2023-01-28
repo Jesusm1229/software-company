0657ebec85833739d21a9139e8b3b0fc4223ac75
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-01-28 16:38:10 -0400

    About section

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import MDXComponents from "../components/MDXComponents";
 import MetaData from "../components/MetaData";
 import PageTop from "../components/PageTop";
@@ -18,8 +19,21 @@ import { homeProfileImage } from "../utils/utils"; // not created yet
 import { FiDownload } from "react-icons/fi";
 import Ripples from "react-ripples";
 import Image from "next/image";
+import { gsap } from "gsap/dist/gsap";
 
 export default function About({ about }) {
+  const app = useRef();
+  useEffect(() => {
+    let ctxButton = gsap.context(() => {
+      gsap.to(".enter__bg", {
+        scale: 1.05,
+        repeat: -1,
+        yoyo: true,
+      });
+    }, app);
+    return () => {};
+  }, []);
+
   return (
     <>
       <MetaData
@@ -30,129 +44,92 @@ export default function About({ about }) {
       />
 
       <section className="pageTop">
-        <AnimatedDiv
-          variants={opacityVariant}
-          className={` blog-container prose-sm  3xl:prose-lg`}
-        >
-          {/* <MDXRemote
-            {...about.content}
-            frontmatter={about.meta}
-            components={MDXComponents}
-          /> */}
-          <PageTop pageTitle="Jesús Medina" className></PageTop>
-          <div className="relative dark:bg-dark Primary dark:text-gray-100">
-            <div className="max-w-4xl 2xl:max-w-5xl 3xl:max-w-7xl mx-auto">
-              <motion.section
-                initial="hidden"
-                whileInView="visible"
-                variants={FadeContainer}
-                viewport={{ once: true }}
-                className="grid place-content-center min-h-screen"
-              >
-                <div className="w-full relative mx-auto flex flex-col items-start gap-10">
-                  <motion.div
-                    variants={popUp}
-                    className="relative w-54 h-54 xs:w-54 xs:h-54  flex justify-start items-center rounded-full p-3 "
-                  >
-                    {/*  /* A component that I created to handle images. It is not important for this
+        <div className="relative dark:bg-dark Primary dark:text-gray-100">
+          <div className="max-w-4xl 2xl:max-w-5xl 3xl:max-w-7xl mx-auto">
+            <motion.section
+              initial="hidden"
+              whileInView="visible"
+              variants={FadeContainer}
+              viewport={{ once: true }}
+              className="grid place-content-center min-h-screen"
+            >
+              <div className="w-full relative mx-auto flex flex-col items-start gap-10">
+                <motion.div
+                  variants={popUp}
+                  className="relative w-3/5 h-screen xs:w-screen xs:h-screen flex justify-center items-center rounded-full p-3 "
+                >
+                  {/*  /* A component that I created to handle images. It is not important for this
                   question.  */}
-                    <Image
-                      src={homeProfileImage}
-                      className="-ml-40 rounded-full shadow filter saturate-0"
-                      width={333}
-                      height={333}
-                      alt="cover Profile Image"
-                      quality={75}
-                      priority={true}
-                    />
 
-                    <div className="flex flex-row-reverse h-full min-h-screen">
-                      <svg className="-ml-20">
+                  <div className="flex flex-col gap-10">
+                    <div className="relative flex flex-row-reverse h-full ">
+                      <Image
+                        src={homeProfileImage}
+                        className=" rounded-full shadow filter saturate-0"
+                        width={333}
+                        height={333}
+                        alt="cover Profile Image"
+                        quality={75}
+                        priority={true}
+                      />
+                      <svg className="-mr-28">
                         <ellipse
-                          rx="150"
+                          opacity="0.5"
+                          rx="140"
                           ry="150"
                           cx="50%"
                           cy="50%"
-                          fill="white"
-                          stroke="blue"
+                          fill="#0081B4"
+                          /* stroke="blue" */
                           stroke-width="3"
                         />
                       </svg>
-                      <svg className="-ml-20">
+                      <svg className="-mr-20">
                         <ellipse
-                          rx="150"
-                          ry="170"
+                          opacity="0.3"
+                          rx="120"
+                          ry="120"
                           cx="50%"
                           cy="50%"
-                          fill="white"
-                          stroke="green"
+                          fill="#2DCDDF"
+                          /*  stroke="green" */
                           stroke-width="3"
                         />
                       </svg>
-                      <svg className="-ml-20">
+                      <svg className="-mr-28">
                         <ellipse
-                          rx="150"
-                          ry="190"
+                          opacity="0.3"
+                          rx="100"
+                          ry="100"
                           cx="50%"
                           cy="50%"
-                          fill="white"
-                          stroke="red"
+                          fill="#86E5FF"
+                          /* stroke="red" */
                           stroke-width="3"
                         />
                       </svg>
+                      <div className="asbsolute flex flex-col gap-1 bottom-0 left-0 h-16 w-16 -mt-50">
+                        <motion.h1
+                          variants={opacityVariant}
+                          className="w-full absolute text-5xl lg:text-6xl font-bold font-sarina bottom-0 left-0 h-32"
+                        >
+                          JESUS MEDINA
+                        </motion.h1>
+                      </div>
                     </div>
-                  </motion.div>
-                  <motion.h1
-                    variants={opacityVariant}
-                    className="text-9xl lg:text-9xl font-bold font-montserrat"
-                  >
-                    JESUS MEDINA
-                  </motion.h1>
-
-                  <div className="w-full flex flex-col p-5 gap-3 select-none text-center ">
-                    <div className="flex flex-col gap-1">
-                      <motion.h1
-                        variants={opacityVariant}
-                        className="text-9xl lg:text-9xl font-bold font-montserrat"
-                      >
-                        JESUS MEDINA
-                      </motion.h1>
-                      <motion.p
-                        variants={opacityVariant}
-                        className="font-medium text-xs md:text-sm lg:text-lg text-gray-500"
-                      >
-                        React Developer, Competitive Programmer
-                      </motion.p>
-                    </div>
-
                     <motion.p
                       variants={opacityVariant}
-                      className=" text-slate-500 dark:text-gray-300 font-medium text-sm md:text-base text-center"
+                      className="pt-6 font-medium text-xs md:text-sm lg:text-lg text-[#383838] dark:text-gray-200"
                     >
-                      I am currently pursuing my Bachelor Degree in Computer
-                      Science. I can code in Python, C, C++, etc.
+                      React Developer, Competitive Programmer
                     </motion.p>
                   </div>
-
-                  <motion.div
-                    className="rounded-md overflow-hidden"
-                    variants={popUp}
-                  >
-                    <Ripples className="w-full" color="rgba(0, 0, 0, 0.5)">
-                      <button
-                        className="flex items-center gap-2 px-5 py-2 border rounded-md border-gray-500 dark:border-gray-400 select-none  hover:bg-gray-100 dark:hover:bg-neutral-800 outline-none"
-                        onClick={() => window.open("/resume")}
-                      >
-                        <FiDownload />
-                        <p>Resume</p>
-                      </button>
-                    </Ripples>
-                  </motion.div>
-                </div>
-              </motion.section>
-            </div>
+                </motion.div>
+              </div>
+            </motion.section>
           </div>
-        </AnimatedDiv>
+        </div>
+
         {/*  <Support /> */}
       </section>
     </>
